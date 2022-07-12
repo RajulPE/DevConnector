be25b90e71bcb4da6ce706f2615fd5b79b220472
RajulPE <76816824+RajulPE@users.noreply.github.com>
2022-07-12 22:47:44 +0530

    Final Commit

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import PostItem from './PostItem';
+import PostForm from './PostForm';
+import { getPosts } from '../../actions/post';
+
+const Posts = ({ getPosts, post: { posts } }) => {
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  return (
+    <section className="container post-container">
+      <div className="post-flex">
+        <div className="post-detail">
+          <h1 className="large text-primary">Posts</h1>
+          <p className="lead">
+            <i className="fas fa-user" /> Welcome to the community
+          </p>
+          <PostForm />
+        </div>
+        <div className="posts">
+          {posts.map((post) => (
+            <PostItem key={post._id} post={post} />
+          ))}
+        </div>  
+      </div>
+    </section>
+  );
+};
+
+Posts.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  post: state.post
+});
+
+export default connect(mapStateToProps, { getPosts })(Posts);
